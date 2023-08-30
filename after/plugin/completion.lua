5e211714660dba92d745c22b8ededec379e611f8
David Engelmann <david+4113@satisfilabs.com>
2023-08-30 09:53:25 -0400

    update cody/sg stuff to track tj

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -73,6 +73,13 @@ cmp.setup {
 
     -- ["<tab>"] = false,
     ["<tab>"] = cmp.config.disable,
+    ["<c-a>"] = cmp.mapping.complete {
+      config = {
+        sources = {
+          { name = "cody" },
+        },
+      },
+    },
 
     -- ["<tab>"] = cmp.mapping {
     --   i = cmp.config.disable,
@@ -114,6 +121,7 @@ cmp.setup {
   --        max_item_count
   --        (more?)
   sources = cmp.config.sources({
+    { name = "cody" },
     { name = "nvim_lua" },
     { name = "nvim_lsp" },
     { name = "luasnip" },
@@ -174,6 +182,7 @@ cmp.setup {
         gh_issues = "[issues]",
         tn = "[TabNine]",
         eruby = "[erb]",
+        cody = "[cody]",
       },
     },
   },
